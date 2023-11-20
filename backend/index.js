93f6ac14eec2ac8355a025952febdc9e861c84f5
Akhil K V <64690489+akkv04@users.noreply.github.com>
2023-11-20 12:20:56 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
